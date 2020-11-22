384eecd6daa7d6e5bc4292a038710eb3c8b3820a
isakulaksiz <isakulaksiz@outlook.com>
2020-11-22 17:14:42 +0300

    feat: Add update and insert publisher

--- src/main/java/com/example/demo/controllers/PublisherController.java
+++ src/main/java/com/example/demo/controllers/PublisherController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.models.Book;
 import com.example.demo.models.Publisher;
 import com.example.demo.services.PublisherService;
 
@@ -28,5 +32,33 @@ public class PublisherController {
 		theModel.addAttribute("publishers", thePublisherService);
 		
 		return "publishers/list-publishers";
+	}
+	
+	@GetMapping("/showFormAdd")
+	public String showFormAdd(Model theModel) {
+		
+		Publisher thePublisher = new Publisher();
+		
+		theModel.addAttribute("publisher", thePublisher);
+		
+		return "publishers/publisher-form";
+	}
+	
+	@GetMapping("/showFormUpdate")
+	public String showFormUpdate(@RequestParam("publisherId") int theId, Model theModel) {
+
+		Publisher thePublisher = publisherService.findById(theId);
+		
+		theModel.addAttribute("publisher", thePublisher);		
+		
+		return "publishers/publisher-form";
+	}
+	
+	@PostMapping("/save")
+	public String save(@ModelAttribute("publisher") Publisher thePublisher) {
+		
+		publisherService.save(thePublisher);
+		
+		return "redirect:/publishers/list";
 	}
 }
